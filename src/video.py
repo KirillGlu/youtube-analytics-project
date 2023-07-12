4d276050bfa21eafd5a80db8a110262a13030e3a
Kirill_Glushankov <kiglu7@gmail.com>
2023-07-12 19:06:24 +0500

    add Homework #6

--- src/video.py
+++ src/video.py
@@ -7,12 +7,18 @@ class Video(Channel):
     def __init__(self, video_id: str) -> None:
         """Экземпляр инициализируется id видео. Дальше все данные будут подтягиваться по API."""
         self.video_id = video_id
-        self.video = self.get_service().videos().list(id=self.video_id,
-                                                      part='snippet,statistics,contentDetails,topicDetails').execute()
-        self.id = self.video["items"][0]['id']
-        self.title = self.video["items"][0]['snippet']["title"]
-        self.view_count = self.video["items"][0]['statistics']["viewCount"]
-        self.like_count = self.video["items"][0]['statistics']["likeCount"]
+        try:
+            self.video = self.get_service().videos().list(id=self.video_id,
+                                                          part='snippet,statistics,contentDetails,topicDetails').execute()
+            self.id = self.video["items"][0]['id']
+            self.title = self.video["items"][0]['snippet']["title"]
+            self.view_count = self.video["items"][0]['statistics']["viewCount"]
+            self.like_count = self.video["items"][0]['statistics']["likeCount"]
+        except Exception:
+            self.id = None
+            self.title = None
+            self.view_count = None
+            self.like_count = None
 
     def __str__(self):
         return f"{self.title}"
